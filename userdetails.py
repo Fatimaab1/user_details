960ce285f61c1a05232a8571df1772e7672e2b2c
Fatimaab1 <fatimabarkat@outlook.com>
2022-08-01 20:05:13 +0100

    update

--- userdetails.py
+++ userdetails.py
@@ -1,23 +1,21 @@
-def users_details():
-    firstName = input('What is your first name?')
-    print(firstName)
+firstName = input('What is your first name?')
+print(firstName)
 
-    lastName = input('What is your last name?')
-    print(lastName)
+lastName = input('What is your last name?')
+print(lastName)
 
-    age = int(input('How old are you?'))
-    print(age)
+age = int(input('How old are you?'))
+print(age)
 
-    address = input('What is your address?')
-    print(address)
+address = input('What is your address?')
+print(address)
 
-    houseNumber = input("enter door number")
-    if houseNumber.isdigit():
-        print(houseNumber)
-    else:
-        print("house number entered is invalid")
+houseNumber = input("enter door number")
+if houseNumber.isdigit():
+    print(houseNumber)
+else:
+    print("house number entered is invalid")
 
 
-#users_details()
-#def print_users_details(firstName, lastName, age, address, houseNumber):
-    print("Hello", firstNameame + "!\n\nYou are", str(age), "years old \n\nAnd live in", address, "Your house number is", houseNumber)
+print("Hi", firstName, lastName + "!\n\nYou're", str(age), "\n\nYou live in", address, "And you'r door number is",
+      houseNumber)
