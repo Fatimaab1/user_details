0839aeacc1c3c65ecd14e149e3faf594c22bebbd
Fatimaab1 <fatimabarkat@outlook.com>
2022-08-02 11:47:56 +0100

    data collections

---
+++ Collections.py
@@ -0,0 +1,22 @@
+# Data Collection
+## Lists, Tuples, Dictionaries
+
+#### Lists
+# What are lists?
+# correct syntax []
+# lists are mutable
+# indexing; same concept applies
+
+shopping_list =["bat", "milk", "bread"]
+print(shopping_list)
+
+# find out the type of shopping list
+print(type(shopping_list))
+print(type("bat"))
+print(type("milk"))
+print(type("bread"))
+
+# find out the len of shopping list
+print(len(shopping_list))
+
+
