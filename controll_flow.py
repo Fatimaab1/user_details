2fd24903501951a3f10b477cdd6be95d91c21f3d
Fatimaab1 <fatimabarkat@outlook.com>
2022-08-03 12:44:02 +0100

    functions

--- controll_flow.py
+++ controll_flow.py
@@ -44,26 +44,76 @@
 # create a dictionary with 6 key value pairs
 # NOTE: "key" : "value",
 
-colour_list = {
-    "colour1": "red",
-    "colour2": "blue",
-    "colour3": "pink",
-    "colour4": "yellow",
-    "colour5": "orange",
-    "colour6": "purple"
-}
+#colour_list = {
+   # "colour1": "red",
+   # "colour2": "blue",
+    #"colour3": "pink",
+    #"colour4": "yellow",
+    #"colour5": "orange",
+    #"colour6": "purple"
+#}
 # use for loop to iterate through it
-for item in colour_list:
-    print(item)
+#for item in colour_list:
+   # print(item)
 
 # print only keys
-print(colour_list.keys())
+#print(colour_list.keys())
 
 # print only value
-print(colour_list.values())
+#print(colour_list.values())
 
 # print key with matching value
-for key, value in colour_list.items():
-    print(key,value)
+#for key, value in colour_list.items():
+    #print(key,value)
 
 
+# use case of multiple conditions
+# create a list with int value 1-4
+#data_list = [1, 2, 3, 4]
+# iterate through the list using for loop
+#for number in data_list:
+
+# find 3 and print if found
+  #  if number == 3:
+     #   print("3")
+# or else list number greater than 3 print gone too far
+   # elif number > 3:
+           # print("gone too far")
+# otherwise print too soon
+#else:
+    #print("too soon")
+
+
+# while loop
+# while loop is mostly used as a monitor rather than handling
+
+#numbers = 0
+# iterate while number is less than 10
+#while numbers < 10:
+
+# print the number with message stating it's working
+    #print(f'its working --> {numbers}')
+
+# add +1 in each iteration
+#numbers += 1
+
+#age = input("please Enter your age")
+# print a message stating your age is user input
+#print(f" your age is {age}")
+
+user_prompt = True
+
+while user_prompt:
+    age = input("Please enter your age ")
+    if age.isdigit() and int(age) < 117:
+        user_prompt = False #stop prompting the user (we received what we needed)
+    else:
+        print("please enter numbers only")
+print(f"Your age is {age}")
+
+
+
+# using the above use case also check if the user age is less than 117 years
+# before you and the while loop
+# in order to do that you may need to use casting -
+# in other words may need to convert age to int
---
+++ functions.py
@@ -0,0 +1,25 @@
+# what is a function
+# why do we need it
+# DRY - DO NOT REPEAT YOURSELF
+# syntax def function_name():
+
+# first iteration
+#def greet_user():
+  #  print("welcome")
+#greet_user()
+
+# greet the user with their name
+# prompt the user to enter their name
+# display a welcome message together with their name
+
+#def greet_user(name):
+   #print("welcome " + name)
+
+#greet_user("her")
+
+# create a function called add that takes 2 arguments as int
+# add both values and display the result
+
+def add(age, height):
+    return (age + height)
+print(add(3,4))
